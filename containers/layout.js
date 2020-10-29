008158f1b97e923afb2f89dd05d532c37cee520a
Facundo Brahim <facundo.e.brahim@gmail.com>
2020-10-29 19:49:49 -0300

    Context - Layout

--- containers/layout.js
+++ containers/layout.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useUser } from "@/lib/useUser";
+import { AuthContext } from "@/contexts/AuthContext";
 
 export default function Layout({ children }) {
-  const { user } = useUser();
+  const { user, loadingAuthState } = useContext(AuthContext);
+  console.log(user, loadingAuthState);
 
   return <>{children}</>;
 }
